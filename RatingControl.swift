c9939a27cea4dfe5fffbc1efa3886138cf29b2d8
filin0wl <nastia-dogf@mail.ru>
2020-04-19 15:02:53 -0700

    Add RatingControl.swift

---
+++ RatingControl.swift
@@ -0,0 +1,15 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by 123 on 19.04.2020.
+//  Copyright © 2020 Apple Inc. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+ 
+
+}

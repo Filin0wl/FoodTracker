b6adddd8d3b628f266c8a5f1b159d6e52457eb9c
filin0wl <nastia-dogf@mail.ru>
2020-04-19 17:02:31 -0700

    not yet done
    need to fix the layout

--- ViewController.swift
+++ ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var photoimageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
     
     
     override func viewDidLoad() {
@@ -60,9 +61,6 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
         present(imagePickerController, animated: true, completion: nil)
     }
     
-    @IBAction func setDefaultLablText(_ sender: UIButton) {
-        mealNameLabel.text = "Default text"
-        
-    }
+
 }
 
